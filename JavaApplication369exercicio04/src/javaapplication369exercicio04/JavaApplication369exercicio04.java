e3fdf2e1a6051defabb3d259d14954227b281a2f
murilo167 <murilopasso95@gmail.com>
2025-05-20 16:17:06 -0300

    Update JavaApplication369exercicio04.java

--- JavaApplication369exercicio04/src/javaapplication369exercicio04/JavaApplication369exercicio04.java
+++ JavaApplication369exercicio04/src/javaapplication369exercicio04/JavaApplication369exercicio04.java
@@ -1,21 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication369exercicio04;
 
-/**
- *
- * @author aluno.saolucas
- */
 public class JavaApplication369exercicio04 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o número de strings:");
+        int n = ler.nextInt();
+        ler.nextLine();  // Para consumir a nova linha após a leitura de um inteiro
+
+        String[] strings = new String[n];
+
+        System.out.println("Digite as strings:");
+        for (int i = 0; i < n; i++) {
+            strings[i] = ler.nextLine();
+        }
+
+        String resultado = stringComMaiorNumeroDeVogais(strings);
+        System.out.println("A string com o maior número de vogais é: " + resultado);
+    }
+
+    public static String stringComMaiorNumeroDeVogais(String[] array) {
+        String string
+
+      
     }
     
 }
